5b7c5b6ad75b75ca36522a8b90b87aaeec3ab58b
ava krishn <avakrishn@gmail.com>
2018-09-29 13:12:56 -0700

    format Card.js

--- click-game/src/components/Card.js
+++ click-game/src/components/Card.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-{/*import the Card.css styling*/}
-import '../styles/Card.css';
+import React, { Component } from "react";
+{
+  /*import the Card.css styling*/
+}
+import "../styles/Card.css";
 
+class Card extends Component {
+  constructor() {
+    super();
+  }
 
-class Card extends Component{
-    constructor(){
-        super()
-    } 
-    
-      render(){
-        {/* return an image tag that gets the props from parent via the spread operator, which will also bind all the functions from the parent onto the child*/}
-        return (
-          <img {...this.props} className="imageSize" src={this.props.imageSrc} ></img>
-        );
-      }
+  render() {
+    {
+      /* return an image tag that gets the props from parent via the spread operator, which will also bind all the functions from the parent onto the child*/
+    }
+    return (
+      <img {...this.props} className="imageSize" src={this.props.imageSrc} />
+    );
+  }
 }
 
 export default Card;
 
-{/* {...this.props} binds all the functions from the parent onto the child */}
+{
+  /* {...this.props} binds all the functions from the parent onto the child */
+}
